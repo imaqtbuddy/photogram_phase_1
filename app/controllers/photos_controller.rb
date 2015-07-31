1ab31253edc365718bbc8b633016a65564fb8ac1
Andi Fuess <afuess@chicagobooth.edu>
2015-07-31 13:51:26 -0500

    Commit_AF

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,12 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+def show
+  @photoid=params["id"]
+  @photo_link=Photo.find(@photoid).source
+  @photo_caption=Photo.find(@photoid).caption
+  render("show.html.erb")
+end
 
 end
+
